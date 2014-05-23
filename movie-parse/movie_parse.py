8c0dc35acacce0b07895d08fb3b59e6be9cd51e7
asbhat <aditya.s.bhat@gmail.com>
2014-05-22 20:41:54 -0400

    adjusted parse file and output

--- movie-parse/movie_parse.py
+++ movie-parse/movie_parse.py
@@ -6,7 +6,7 @@ labels = ['product/productId', 'review/text', 'review/helpfulness', \
 def parse_file(fileToOpen, labels):
 	openFile = open(fileToOpen, 'r')
 	outputData = []
-	outputRow = [0 for x in range(4)]
+	outputRow = [0 for x in range(5)]
 	for ln in openFile:
 		splitln = ln.split()
 		if len(splitln) > 1:
@@ -22,20 +22,18 @@ def parse_file(fileToOpen, labels):
 			else:
 				helpPct = float(helpList[0])/float(helpList[1])
 			outputRow[2] = helpPct
+			outputRow.insert(3,helpList[1])
 			outputData.append(outputRow)
-			outputRow = [0 for x in range(4)]
+			outputRow = [0 for x in range(5)]
 
 	return outputData
 
 if __name__ == '__main__':
 	outputData = parse_file(fileToOpen, labels)
 	outputFile = open('parsedmoviefile.txt', 'w')
-	for la in labels:
-		outputFile.write(la + '\t')
-	outputFile.write('\n')
+	
+	outputFile.write('ASIN\tText\tHelpfulness\tHCount\tScore\n')
 
 	for ln in outputData:
-		for elem in ln:
-			outputFile.write(str(elem) + '\t')
-		outputFile.write('\n')
+		outputFile.write('{0}\t{1}\t{2}\t{3}\t{4}\n'.format(ln[0],ln[1],ln[2],ln[3],ln[4]))
 	outputFile.close()
